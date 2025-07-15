1220a0195e7df990dd3adcc88d707f7f3c8af7e1
Shoaib khan <maculatechtesting@gmail.com>
2025-07-15 15:17:43 +0530

    new fixes for media error

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,21 @@
+// import { withPayload } from '@payloadcms/next/withPayload'
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'http',
+//         hostname: 'localhost',
+//         port: '3000',
+//         pathname: '/api/media/file/**',
+//       },
+//     ],
+//   },
+// }
+
+// export default withPayload(nextConfig, { devBundleServerPackages: false })
+
 import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
@@ -9,6 +27,16 @@ const nextConfig = {
         hostname: 'localhost',
         port: '3000',
         pathname: '/api/media/file/**',
+      },
+      {
+        protocol: 'https',
+        hostname: `${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`,
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: `${process.env.AWS_S3_BUCKET}.s3.amazonaws.com`,
+        pathname: '/**',
       },
     ],
   },
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,7 +1,54 @@
-// storage-adapter-import-placeholder
+// // storage-adapter-import-placeholder
+// import { mongooseAdapter } from '@payloadcms/db-mongodb'
+// import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
+// import { lexicalEditor } from '@payloadcms/richtext-lexical'
+// import path from 'path'
+// import { buildConfig } from 'payload'
+// import { fileURLToPath } from 'url'
+// import sharp from 'sharp'
+
+// import { Users } from './collections/Users'
+// import { Media } from './collections/Media'
+// import { Header } from './collections/Header'
+// import { Footer } from './collections/Footer'
+// import { HomePage } from './collections/HomePage'
+// import { ServicesPage} from './collections/ServicesPage'
+// import { AboutPage } from './collections/AboutPage'
+// import { GalleryPage } from './collections/Gallery'
+// import { ContactPage } from './collections/ContactPage'
+// import { GetQuotePage } from './collections/GetQuote'
+
+// const filename = fileURLToPath(import.meta.url)
+// const dirname = path.dirname(filename)
+
+// export default buildConfig({
+//   admin: {
+//     user: Users.slug,
+//     importMap: {
+//       baseDir: path.resolve(dirname),
+//     },
+//   },
+//   collections: [Users, Media],
+//   globals: [Header, Footer, HomePage, ServicesPage, AboutPage, GalleryPage, ContactPage, GetQuotePage],
+//   editor: lexicalEditor(),
+//   secret: process.env.PAYLOAD_SECRET || 'c86f5ffb890af0949f8c9a90',
+//   typescript: {
+//     outputFile: path.resolve(dirname, 'payload-types.ts'),
+//   },
+//   db: mongooseAdapter({
+//     url: process.env.DATABASE_URI || '',
+//   }),
+//   sharp,
+//   plugins: [
+//     payloadCloudPlugin(),
+//     // storage-adapter-placeholder
+//   ],
+// })
+
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { s3Storage } from '@payloadcms/storage-s3'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
@@ -41,7 +88,20 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    s3Storage({
+      collections: {
+        media: {
+          prefix: 'media',
+        },
+      },
+      bucket: process.env.S3_BUCKET_NAME!,
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+        },
+        region: process.env.S3_REGION!,
+      },
+    }),
   ],
 })
-
